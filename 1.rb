8aad92622cc38560b428509a9fd34481631d8ba2
MariaTikhonova <ritz.rkraft@gmail.com>
2017-08-22 16:34:43 +0300

    Add files via upload

---
+++ 1.rb
@@ -0,0 +1,12 @@
+numbers=[]
+newar=[]
+i=0
+while newar.length<5
+while i<=99 
+i+=1
+numbers<<i
+newar <<rand(numbers.length)
+end
+
+p newar
+
---
+++ 2.rb
@@ -0,0 +1,11 @@
+numbers=[]
+newar=[]
+i=0
+while i<=99 
+i+=1
+numbers<<i
+end
+
+puts newar<<numbers.sample(5)
+
+
